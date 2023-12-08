5e76c3880e1ce834c6c65b3a4bd5593a6b48fec9
MA MINSU <85151350+ma-minsu@users.noreply.github.com>
2023-12-08 10:02:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const api_id = process.env.TEKEAT_MAP_ID;
-const api_key = process.env.TEKEAT_MAP_KEY;
+const api_id = "${{ secrets.TEKEAT_MAP_ID }}";
+const api_key = "${{ secrets.TEKEAT_MAP_KEY }}";
 
 let map; // 전역 변수로 지도 객체 선언
 
